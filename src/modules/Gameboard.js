93f562cd75d57bfb08dcf88510e4910e5ee8ac93
KarimElsayad247 <karimelsayad1999@gmail.com>
2022-11-20 15:37:27 +0200

    Implement ship placement logic

--- src/modules/Gameboard.js
+++ src/modules/Gameboard.js
@@ -1,7 +1,24 @@
 const Cell = () => {
   let isHit = false;
-  let shipIndex = 0;
-  return { isHit, shipIndex };
+  let ship = null;
+
+  const assignShip = (shipToPlace) => {
+    ship = shipToPlace;
+  };
+
+  const hit = () => {
+    isHit = true;
+  };
+
+  const containsShip = () => {
+    return ship !== null;
+  };
+
+  const getShip = () => {
+    return ship;
+  };
+
+  return { isHit, getShip, assignShip, hit, containsShip };
 };
 
 export default function () {
@@ -9,12 +26,72 @@ export default function () {
   const gridHight = 10;
 
   const grid = Array.from(Array(gridHight), () =>
-    new Array(gridWidth).map(() => {
-      Cell();
-    })
+    new Array(gridWidth).fill().map((_) => Cell())
   );
+
+  const isPlaceable = (ship, x, y, orientation) => {
+    // Ensure there is enough space to put the ship
+    // Ensure there is enough space to put the ship
+    if (orientation === "horizontal") {
+      if (ship.length + x > gridWidth) {
+        return false;
+      }
+    }
+
+    if (orientation === "vertical") {
+      if (ship.length + y > gridHight) {
+        return false;
+      }
+    }
+
+    return true;
+
+    // switch (orientation) {
+    //   case "horizontal":
+    //     if (ship.length + x > gridWidth) {
+    //       return false;
+    //     }
+    //   case "vertical":
+    //     if (ship.length + y > gridHight) {
+    //       return false;
+    //     }
+    //     break;
+    //   default:
+    //     break;
+    // }
+  };
+
+  const placeShip = (ship, x, y, orientation) => {
+    if (!isPlaceable(ship, x, y, orientation)) {
+      throw Error("Can't place ship here!");
+    }
+
+    switch (orientation) {
+      case "horizontal":
+        for (let i = x; i < ship.length + x; i++) {
+          const cell = grid[y][i];
+          cell.assignShip(ship);
+        }
+        break;
+
+      case "vertical":
+        for (let i = y; i < ship.length + y; i++) {
+          const cell = grid[i][x];
+          cell.assignShip(ship);
+        }
+        break;
+
+      default:
+        break;
+    }
+  };
 
   const receiveAttack = (x, y) => {};
 
-  return { receiveAttack };
+  const containsShip = (x, y) => {
+    const cell = grid[y][x];
+    return cell.containsShip();
+  };
+
+  return { receiveAttack, placeShip, isPlaceable, containsShip };
 }
--- test/Gameboard.test.js
+++ test/Gameboard.test.js
@@ -1,5 +1,34 @@
 import Gameboard from "../src/modules/Gameboard";
+import Ship from "../src/modules/Ship";
 
-test("Places ship on board", () => {});
+test("Places ship horizontally on board", () => {
+  const board = Gameboard();
+  const ship = Ship(4);
+  board.placeShip(ship, 0, 0, "horizontal");
+  for (let i = 0; i < 4; ++i) {
+    expect(board.containsShip(i, 0)).toBeTruthy();
+  }
+});
+
+test("Places ship vertically on board", () => {
+  const board = Gameboard();
+  const ship = Ship(4);
+  board.placeShip(ship, 0, 0, "vertical");
+  for (let i = 0; i < 4; ++i) {
+    expect(board.containsShip(0, i)).toBeTruthy();
+  }
+});
+
+test("Raises error when attempting to place ship out of bound vertically", () => {
+  const board = Gameboard();
+  const ship = Ship(4);
+  expect(() => board.placeShip(ship, 0, 9, "vertical")).toThrow();
+});
+
+test("Raises error when attempting to place ship out of bound horizontally", () => {
+  const board = Gameboard();
+  const ship = Ship(4);
+  expect(() => board.placeShip(ship, 9, 0, "horizontal")).toThrow();
+});
 
 test("Receives attack on a cell", () => {});
